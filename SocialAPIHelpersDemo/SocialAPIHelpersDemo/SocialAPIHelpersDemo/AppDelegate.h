e20f69f008f6f32de381eaa0b3093b03e6f91141
shu223 <shuichi0526@gmail.com>
2013-10-20 12:52:33 +0900

    Initial Commit

---
+++ SocialAPIHelpersDemo/SocialAPIHelpersDemo/SocialAPIHelpersDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SocialAPIHelpersDemo
+//
+//  Created by shuichi on 10/20/13.
+//  Copyright (c) 2013 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SocialAPIHelpersDemo/SocialAPIHelpersDemo/SocialAPIHelpersDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SocialAPIHelpersDemo
+//
+//  Created by shuichi on 10/20/13.
+//  Copyright (c) 2013 Shuichi Tsutsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
